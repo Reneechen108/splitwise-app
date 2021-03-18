77027a9ce056d08b947ddeb1b6d7348a022f142c
Reneechen108 <reneechen108@gmail.com>
2021-03-18 17:26:02 +0800

    add pool connection

--- backend/connection/db.js
+++ backend/connection/db.js
@@ -5,11 +5,8 @@ const db = mysql.createConnection({
     host: 'us-cdbr-east-03.cleardb.com',
     user: 'ba265b1db19272',
     password: 'a9c30c6a',
-    // password: 'password',
-    database: 'heroku_c8d35e15a098b78',
-    // insecureAuth : true
+    database: 'heroku_c8d35e15a098b78'
 });
-// cmpe202db.chnvttzxfbpw.us-west-1.rds.amazonaws.com
 
 db.connect(function(err) {
     if(err){
@@ -18,9 +15,28 @@ db.connect(function(err) {
         return false;
     }
     else{
-      console.log("Succesfully connect to DB")
+      console.log("Succesfully connect to DB without pool")
     }
     
 });
+
+// const db = mysql.createPool({
+//     connectionLimit: 100,
+//     host: 'us-cdbr-east-03.cleardb.com',
+//     user: 'ba265b1db19272',
+//     password: 'a9c30c6a',
+//     database: 'heroku_c8d35e15a098b78'
+// });
+
+// db.getConnection(function(err) {
+//     if(err){
+//         console.log('DB error');
+//         throw err;
+//         return false;
+//     }
+//     else{
+//       console.log("Succesfully connect to DB with pool")
+//     }
+// });
 
 module.exports = db;
